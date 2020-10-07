26c791204b256c08faa084a49d88c0f841256704
Krystal Cheung <krycheung@gmail.com>
2020-10-07 17:50:55 -0400

    setup few screens

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import AudioButton from "./AudioButton";
 
-export default function OriginalTextPage() {
+export default function Subjects({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Team G Capstone!</Text>
-      <AudioButton />
+      <Text>Choose a Subject!</Text>
       <StatusBar style="auto" />
+      <Button title="Math" onPress={() => navigation.navigate("Shapes")} />
+      <Button title="History" />
     </View>
   );
 }
