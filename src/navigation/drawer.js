65475d63aa3253cc0f1c7dee9f09349c56730723
Krystal Cheung <krycheung@gmail.com>
2020-10-14 14:39:03 -0400

    got a sidebar

---
+++ src/navigation/drawer.js
@@ -0,0 +1,14 @@
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+
+// const Drawer = createDrawerNavigator();
+
+// function MyDrawer() {
+//   return (
+//     <Drawer.Navigator
+//       drawerContent={(props) => <CustomDrawerContent {...props} />}
+//     >
+//       <Drawer.Screen name="Feed" component={Feed} />
+//       <Drawer.Screen name="Notifications" component={Notifications} />
+//     </Drawer.Navigator>
+//   );
+// }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -10,19 +10,24 @@ import ShapesAnswer from "../components/ShapesAnswer";
 import Subjects from "../components/Subjects";
 import WelcomePage from "../components/WelcomePage";
 import MainMenu from "../components/MainMenu";
-import ColorSortGame from '../components/ColorSortGame';
+import ColorSortGame from "../components/ColorSortGame";
 
 import UserStats_PT from "../components/UserStats_PT";
 import UserStats_Student from "../components/UserStats_Student";
-import SignUp from "../components/SignUp";
-
 import AudioButton from "../components/AudioButton";
+
+import SignUp from "../components/SignUp";
 import SignIn from "../components/SignIn";
 import LoadingScreen from "../components/LoadingScreen";
 //Firebase
 import * as firebase from "firebase";
 import apiKeys from "../../config/keys";
 import parentEditProfile from "../components/parentEditProfile";
+
+//Drawer
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
 
 const Stack = createStackNavigator();
 
@@ -31,69 +36,68 @@ if (!firebase.apps.length) {
 }
 const Navigation = () => (
   <NavigationContainer>
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen
         name="LoadingScreen"
         component={LoadingScreen}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="WelcomePage"
         component={WelcomePage}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="SignUp"
         component={SignUp}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="SignIn"
         component={SignIn}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="MainMenu"
         component={MainMenu}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="Subjects"
         component={Subjects}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="Shapes"
         component={Shapes}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="ColorSortGame"
         component={ColorSortGame}
-        options={{ headerShown: false}}
-        />
-      <Stack.Screen
+        options={{ headerShown: false }}
+      />
+      <Drawer.Screen
         name="ShapesAnswer"
         component={ShapesAnswer}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
-      name= "parentEditProfile"
-      component={parentEditProfile}
-      options={{headerShown: false}}
+      <Drawer.Screen
+        name="parentEditProfile"
+        component={parentEditProfile}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="UserStats_PT"
         component={UserStats_PT}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
+      <Drawer.Screen
         name="UserStats_Student"
         component={UserStats_Student}
         options={{ headerShown: false }}
       />
-
-    </Stack.Navigator>
+    </Drawer.Navigator>
   </NavigationContainer>
 );
 
